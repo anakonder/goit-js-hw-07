7d5a3ba4ec60be0b8cbaf2e51cc23280497c1069
anakonder <120507138+anakonder@users.noreply.github.com>
2023-05-06 17:30:06 +0300

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryList = document.querySelector('.gallery')
+
+const galleryMarcup = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="#" rel="noopener noreferrer">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+    </a>
+    </li>`
+).join('');
+
+galleryList.innerHTML = galleryMarcup;
+// console.log(galleryMarcup)
+console.log(galleryList)
+ 
+let lightbox;
+
+galleryList.addEventListener('click', (event) => {
+    if (event.target.nodeName === 'IMG') { 
+        const imgSrc = event.target.dataset.source;
+        lightbox = basicLightbox.create(`<img width="1280" height="855" src="${imgSrc}">`)
+        lightbox.show()
+    }
+})
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === "Escape" && lightbox) {
+        lightbox.close()
+    }
+})
+    
+   
